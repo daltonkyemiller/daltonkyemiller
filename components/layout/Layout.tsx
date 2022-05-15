7a0a29219a7777503a2edb7c396e25e7cdcf9dc2
Dalton Miller <dalton.kye.miller@gmail.com>
2022-05-15 10:50:11 -0500

    feat(navbar): mobile nav, implemented framer-motion

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -7,7 +7,6 @@ type Props = {
 };
 
 const Layout: React.FC<Props> = (props: Props) => {
-
     return (
         <>
             <Nav/>
--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import { motion } from 'framer-motion';
+import React, { useRef, useState } from 'react';
 
 type Props = {};
 
 const Nav: React.FC<Props> = (props: Props) => {
+    const [showNav, setShowNav] = useState<boolean>(true);
+    const navContent = useRef<null | HTMLDivElement>(null);
+
+    const variants = {
+        show: {
+            opacity: [0, 1],
+            transition: {duration: .25}
+        },
+        hide: {
+            opacity: [1, 0],
+            transition: {duration: .25}
+
+        }
+    };
 
     return (
-        <nav>
-            <h1>hi</h1>
+        <nav
+            className="flex flex-col w-screen h-screen items-center justify-center bg-blue-50 text-center">
+            <button className="absolute top-10" onClick={() => setShowNav(!showNav)}>Click</button>
+            <motion.div className="flex flex-col items-center"
+                        ref={navContent}
+                        initial={'fadeIn'}
+                        variants={variants}
+                        animate={showNav ? 'show' : 'hide'}
+            >
+                <h1 className="font-brand font-semibold text-5xl">DKM</h1>
+                <div className="border-t border-slate-400 w-[calc(100%+20px)] my-2"/>
+                <ul className="flex flex-col text-xl font-sans gap-4 ">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/resume">Resume</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </motion.div>
         </nav>
     );
 };
