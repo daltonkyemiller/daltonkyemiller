87f8a2f0d66e4c6be6c36a7eab99905388eb542d
Dalton Miller <dalton.kye.miller@gmail.com>
2022-07-11 09:58:19 -0500

    upgrade(next,react,types)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = async () => ({
     reactStrictMode: true,
+    experimental: {
+        images: {
+            allowFutureImage: true
+        }
+    },
     images: {
         domains: ['picsum.photos']
     }
-};
+});
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetStaticProps, NextPage } from 'next';
 import { PhotoGrid } from '@components';
-import Image from 'next/image';
+import Image from 'next/future/image';
+import plants from '/public/img/temp/plants.jpg';
 
 type HomeProps = {}
 
@@ -8,7 +9,7 @@ const Home: NextPage<HomeProps> = ({}) => {
     return (
         <div className={`flex flex-col justify-center items-center min-h-full`}>
             <h1 className={`text-5xl font-bold tracking-wide`}>WHO AM I?</h1>
-            <Image src={'/img/temp/plants.jpg'} alt={'Plants'}/>
+            <Image src={plants} alt={`Plants`} className={`w-full`}/>
         </div>
     );
 };
